a7d4982134c64d232b7d7eeb9a6d87bd9330132d
Aboubacar TRAORE <traxyax@gmail.com>
2022-08-22 07:55:09 +0000

    Add config.h file

---
+++ config.h
@@ -0,0 +1,161 @@
+/* See LICENSE file for copyright and license details. */
+/* alt-tab configuration */
+static const unsigned int tabModKey 		= 0x40;	/* if this key is hold the alt-tab functionality stays acitve. This key must be the same as key that is used to active functin altTabStart `*/
+static const unsigned int tabCycleKey 		= 0x17;	/* if this key is hit the alt-tab program moves one position forward in clients stack. This key must be the same as key that is used to active functin altTabStart */
+static const unsigned int tabPosY 			= 1;	/* tab position on Y axis, 0 = bottom, 1 = center, 2 = top */
+static const unsigned int tabPosX 			= 1;	/* tab position on X axis, 0 = left, 1 = center, 2 = right */
+static const unsigned int maxWTab 			= 600;	/* tab menu width */
+static const unsigned int maxHTab 			= 200;	/* tab menu height */
+
+/* appearance */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const int user_bh            = 10;        /* 2 is the default spacing around the bar's font */
+static const char *fonts[]          = {
+	"Monofur Nerd Font:size=20",
+	"agave Nerd Font:size=20",
+	"JetBrainsMono Nerd Font:size=20",
+	"monospace:size=20"
+};
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeHid]  = { col_cyan,  col_gray1, col_cyan  },
+};
+
+/* tagging */
+static const char *tags[] = { "DEV", "WWW", "WRK", "TRM", "GFX", "VEO", "BOX", "OER" };
+static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8" };
+static const int momentaryalttags = 0; /* 1 means alttags will show only when key is held down*/
+
+static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+	/* class      instance    title       tags mask    switchtotag     isfloating   monitor    scratch key */
+	{ "Gimp",     NULL,       NULL,       0,            1,1,           -1,        0  },
+	{ "firefox",  NULL,       NULL,       1 << 3,       1,0,           -1,        0  },
+	{ "brave-browser",  NULL,       NULL,       1 << 1,       1,0,           -1,        0  },
+	{ "qutebrowser",  NULL,       NULL,       1 << 1,       1,0,           -1,        0  },
+	{ NULL,       NULL,       "st",            1 << 3,       1,0,           -1,        0  },
+	{ NULL,       NULL,   "scratchpad",   0,            0,1,           -1,       's' },
+};
+
+/* layout(s) */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int attachdirection = 3;    /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+
+static const Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle },
+};
+
+/* key definitions */
+#define MODKEY Mod4Mask
+#define ALTKEY Mod1Mask
+#define SHIFTKEY ShiftMask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "tabbed", "-c", "-r", "2", "st", "-w", "''", NULL };
+
+/*First arg only serves to match against key in rules*/
+//static const char *scratchpadcmd[] = {"s", "tabbed", "-f", "-r", "2", "-g", "1915x600", "st", "-w", "''", "-t", "scratchpad", NULL}; 
+static const char *scratchpadcmd[] = {"s", "tabbed", "-f", "-r", "2", "-g", "1000x800+450+140", "st", "-w", "''", "-t", "scratchpad", NULL}; 
+
+static const Key keys[] = {
+	/* modifier                     key        function        argument */
+	{ ALTKEY,                       XK_F1,      spawn,          SHCMD("dmenu_run -c -n -F -l 10 -g 2 -h 48") },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return,  togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_j,      focusstackvis,  {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstackvis,  {.i = -1 } },
+	{ MODKEY|SHIFTKEY,             XK_j,      focusstackhid,  {.i = +1 } },
+	{ MODKEY|SHIFTKEY,             XK_k,      focusstackhid,  {.i = -1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_x, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	//{ MODKEY|ShiftMask,             XK_j,      aspectresize,   {.i = +24} },
+	//{ MODKEY|ShiftMask,             XK_k,      aspectresize,   {.i = -24} },
+	{ MODKEY,             XK_q,      killclient,     {0} },
+	//{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY,             XK_f,  togglefullscr, {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,             XK_t,  togglealwaysontop, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_a,      togglealttag,   {0} },
+	{ ALTKEY,             		XK_Tab,    altTabStart,	   {0} },
+	{ MODKEY,                       XK_s,      show,           {0} },
+	{ MODKEY|SHIFTKEY,                       XK_s,      hide,           {0} },
+	TAGKEYS(                        XK_ampersand,                      0)
+	TAGKEYS(                        XK_eacute,                      1)
+	TAGKEYS(                        XK_quotedbl,                      2)
+	TAGKEYS(                        XK_apostrophe,                      3)
+	TAGKEYS(                        XK_parenleft,                      4)
+	TAGKEYS(                        XK_minus,                      5)
+	TAGKEYS(                        XK_egrave,                      6)
+	TAGKEYS(                        XK_underscore,                      7)
+	TAGKEYS(                        XK_ccedilla,                      8)
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
+};
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static const Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button1,        togglewin,      {0} },
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
+
